4b4e308d639141880ce5111c1c5f5ceb63d2c08a
Ozer GUL <ozer.gul@etiya.com>
2020-09-23 13:09:09 +0300

    seed type added

--- src/database/seeds/lov.seed.ts
+++ src/database/seeds/lov.seed.ts
@@ -1,6 +1,7 @@
 import { Factory, Seeder } from 'typeorm-seeding';
 import { Connection } from 'typeorm';
 import { LovType } from '../../api/lov/enum';
+import { Lov } from '../../api/lov/lov.entity';
 
 export default class LovSeed implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<any> {
@@ -17,7 +18,7 @@ export default class LovSeed implements Seeder {
         { text: 'Task', type: LovType.TaskType, value: '0' },
         { text: 'Bug', type: LovType.TaskType, value: '1' },
         { text: 'Story', type: LovType.TaskType, value: '2' },
-      ])
+      ] as Lov[])
       .execute();
   }
 }
